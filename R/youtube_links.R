6ae1df65e43c08fe5b3518fd7cb96799cdfb89f7
Joyce Robbins <jtr13@columbia.edu>
2023-11-02 21:24:13 -0400

    minor update

--- R/youtube_links.R
+++ R/youtube_links.R
@@ -5,4 +5,4 @@ path_to_submissions <- "~/Downloads/submissions/"
 files <- list.files(path_to_submissions)
 a <- map(paste0(path_to_submissions, files), read_html)
 links <- map_chr(a, ~.x |> html_element("a") |> html_attr("href"))
-writeLines(links, "MonWedLinks.txt")
+writeLines(links, "notes/MonWedLinks.txt")
